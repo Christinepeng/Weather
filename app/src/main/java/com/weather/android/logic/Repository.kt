dab72a64614fc6d201daa23fce5e825c96f68d10
Yellow Bear <percussionpeng@gmail.com>
2024-02-09 16:10:46 -0800

    Repository.kt: combine try catch at fire() method.

--- app/src/main/java/com/weather/android/logic/Repository.kt
+++ app/src/main/java/com/weather/android/logic/Repository.kt
@@ -7,33 +7,34 @@ import com.weather.android.logic.model.HourlyTimeData
 import com.weather.android.logic.model.Place
 import com.weather.android.logic.network.WeatherNetwork
 import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.CoroutineContext
 
 object Repository {
-    fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
-        val result = try {
-            val placeResponse = WeatherNetwork.searchPlaces(query)
-            if (placeResponse.status == "ok") {
-                val places = placeResponse.places
-                Result.success(places)
-            } else {
-                Result.failure(RuntimeException("response status is ${placeResponse.status}"))
-            }
-        } catch (e: Exception) {
-            Result.failure<List<Place>>(e)
+    fun searchPlaces(query: String) = fire(Dispatchers.IO) {
+        val placeResponse = WeatherNetwork.searchPlaces(query)
+        if (placeResponse.status == "ok") {
+            val places = placeResponse.places
+            Result.success(places)
+        } else {
+            Result.failure(RuntimeException("response status is ${placeResponse.status}"))
         }
-        emit(result)
     }
 
-    fun searchWeather(query: String) = liveData(Dispatchers.IO) {
+    fun searchWeather(query: String) = fire(Dispatchers.IO) {
         Log.v("xd", "query + $query + |")
-        val result = try {
-            val weatherResponse = WeatherNetwork.searchWeather(query)
-            Log.v("xd", weatherResponse.toString().length.toString())
-            Result.success(weatherResponse.timelines.hourly)
-        } catch (e: Exception) {
-            Log.v("xd", "failed" + e.toString())
-            Result.failure<List<HourlyTimeData>>(e)
-        }
-        emit(result)
+        val weatherResponse = WeatherNetwork.searchWeather(query)
+        Log.v("xd", weatherResponse.toString().length.toString())
+        Result.success(weatherResponse.timelines.hourly)
     }
+
+    private fun <T> fire(context: CoroutineContext, block: suspend () -> Result<T>) =
+        liveData<Result<T>>(context) {
+            val result = try {
+                block()
+            } catch (e: Exception) {
+                Log.v("xd", "failed" + e.toString())
+                Result.failure<T>(e)
+            }
+            emit(result)
+        }
 }
