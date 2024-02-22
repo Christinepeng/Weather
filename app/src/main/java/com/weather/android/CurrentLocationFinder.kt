8ace8e726209f5d39796de2daf51a519d94a5704
Yellow Bear <percussionpeng@gmail.com>
2024-02-22 15:17:38 -0800

    Separate "current location function" code to CurrentLocationFinder.kt from MainActivity.kt.

--- app/src/main/java/com/weather/android/CurrentLocationFinder.kt
+++ app/src/main/java/com/weather/android/CurrentLocationFinder.kt
@@ -1,5 +1,70 @@
 package com.weather.android
 
-class CurrentLocationFinder {
+import android.Manifest
+import android.annotation.SuppressLint
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.annotation.RequiresPermission
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.weather.android.databinding.ActivityMainBinding
 
+class CurrentLocationFinder(
+    private val requestPermissionLauncher: ActivityResultLauncher<Array<String>>,
+    private val placesClient: PlacesClient,
+    private val binding: ActivityMainBinding,
+    private val hasOnePermissionGranted: (permissions: Array<String>) -> Boolean,
+) {
+
+    /**
+     * Check whether permissions have been granted or not, and ultimately proceeds to either
+     * request them or runs {@link #findCurrentPlaceWithPermissions() findCurrentPlaceWithPermissions}
+     */
+    @SuppressLint("MissingPermission")
+    fun findCurrentPlace() {
+        if (hasOnePermissionGranted(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        ) {
+            findCurrentPlaceWithPermissions()
+            return
+        } else {
+            requestPermissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+    }
+
+    /**
+     * Fetches a list of [com.google.android.libraries.places.api.model.PlaceLikelihood] instances that represent the Places the user is
+     * most likely to be at currently.
+     */
+    @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_WIFI_STATE])
+    fun findCurrentPlaceWithPermissions() {
+//        setLoading(true)
+        val currentPlaceRequest = FindCurrentPlaceRequest.newInstance(listOf(Place.Field.ADDRESS))
+        val currentPlaceTask = placesClient.findCurrentPlace(currentPlaceRequest)
+        currentPlaceTask.addOnSuccessListener { response: FindCurrentPlaceResponse? ->
+            response?.let {
+//                binding.response.text = StringUtil.stringify(it, isDisplayRawResultsChecked)
+//                binding.title.text = StringUtil.stringify(it)
+                binding.title.text = it.placeLikelihoods[0].place.address
+//                Log.e("xd", "Suc ${StringUtil.stringify(it)}")
+            }
+        }
+        currentPlaceTask.addOnFailureListener { exception: Exception ->
+            exception.printStackTrace()
+            Log.e("xd", "Failed $exception")
+//            binding.response.text = exception.message
+        }
+//        currentPlaceTask.addOnCompleteListener { setLoading(false) }
+    }
 }
--- app/src/main/java/com/weather/android/MainActivity.kt
+++ app/src/main/java/com/weather/android/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var placesClient: PlacesClient
     private lateinit var fieldSelector: FieldSelector
+    private lateinit var currentLocationFinder: CurrentLocationFinder
 
     @SuppressLint("MissingPermission")
     val requestPermissionLauncher =
@@ -37,8 +38,9 @@ class MainActivity : AppCompatActivity() {
             when {
                 permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true || permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true -> {
                     // Only approximate location access granted.
-                    findCurrentPlaceWithPermissions()
+                    currentLocationFinder.findCurrentPlaceWithPermissions()
                 }
+
                 else -> {
                     Toast.makeText(
                         this,
@@ -49,8 +51,18 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    private fun hasOnePermissionGranted(permissions: Array<String>): Boolean =
+        permissions.any {
+            ContextCompat.checkSelfPermission(
+                this,
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -66,8 +78,15 @@ class MainActivity : AppCompatActivity() {
         // Retrieve a PlacesClient (previously initialized - see MainActivity)
         placesClient = Places.createClient(this)
 
+        currentLocationFinder = CurrentLocationFinder(
+            requestPermissionLauncher,
+            placesClient,
+            binding,
+            ::hasOnePermissionGranted
+        )
 
-        findCurrentPlace()
+
+        currentLocationFinder.findCurrentPlace()
 
 
 
@@ -84,7 +103,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-//    private val placeFields: List<Place.Field> get() = listOf(*Place.Field.values())
+    //    private val placeFields: List<Place.Field> get() = listOf(*Place.Field.values())
     private val placeFields: List<Place.Field> get() = listOf(Place.Field.NAME)
 
     private val placeSelectionListener: PlaceSelectionListener
@@ -109,58 +128,10 @@ class MainActivity : AppCompatActivity() {
         fieldSelector.onSaveInstanceState(bundle)
     }
 
-    /**
-     * Check whether permissions have been granted or not, and ultimately proceeds to either
-     * request them or runs {@link #findCurrentPlaceWithPermissions() findCurrentPlaceWithPermissions}
-     */
-    @SuppressLint("MissingPermission")
-    private fun findCurrentPlace() {
-        if (hasOnePermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)) {
-            findCurrentPlaceWithPermissions()
-            return
-        } else {
-            requestPermissionLauncher.launch(
-                arrayOf(
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                )
-            )
-        }
-    }
 
 //    var getCityNameFromAddress() {
 //
 //    }
 
-    /**
-     * Fetches a list of [com.google.android.libraries.places.api.model.PlaceLikelihood] instances that represent the Places the user is
-     * most likely to be at currently.
-     */
-    @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_WIFI_STATE])
-    private fun findCurrentPlaceWithPermissions() {
-//        setLoading(true)
-        val currentPlaceRequest = FindCurrentPlaceRequest.newInstance(placeFields)
-        val currentPlaceTask = placesClient.findCurrentPlace(currentPlaceRequest)
-        currentPlaceTask.addOnSuccessListener { response: FindCurrentPlaceResponse? ->
-            response?.let {
-//                binding.response.text = StringUtil.stringify(it, isDisplayRawResultsChecked)
-//                binding.title.text = StringUtil.stringify(it)
-                binding.title.text = it.placeLikelihoods[0].place.address
-                Log.e("xd", "Suc")
-            }
-        }
-        currentPlaceTask.addOnFailureListener { exception: Exception ->
-            exception.printStackTrace()
-            Log.e("xd", "Failed $exception")
-//            binding.response.text = exception.message
-        }
-//        currentPlaceTask.addOnCompleteListener { setLoading(false) }
-    }
-    private fun hasOnePermissionGranted(vararg permissions: String): Boolean =
-        permissions.any {
-            ContextCompat.checkSelfPermission(
-                this,
-                it
-            ) == PackageManager.PERMISSION_GRANTED
-        }
 }
+
